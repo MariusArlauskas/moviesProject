05f2f4f3e32eff578797bf29f3d9ed35fcf48b10
Marius Arlauskas <arlauskas.mar@gmail.com>
2020-04-10 20:34:05 +0300

    left out files from lasst push

---
+++ moviesFrontEnd/src/components/ProfilePage/MainWall.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="transparent" style="margin-top: 25px" height="100%" width="100%" flat dark>
+    <v-layout class="mainContainer" justify-center row>
+      <v-card class="transparent" flat>MAIN WALL</v-card>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ProfileMainWall"
+};
+</script>
+
+<style scoped>
+</style>
---
+++ moviesFrontEnd/src/components/ProfilePage/MoviesList.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card
+    class="mainContainer transparent"
+    style="margin-top: 25px"
+    height="100%"
+    width="100%"
+    flat
+    dark
+  >
+    <ProfileMoviesFilter />
+    <v-layout style="margin-left: 15%" row>
+      <v-card width="80%" class="transparent" flat>
+        <v-layout row>
+          <v-flex>a</v-flex>
+        </v-layout>
+      </v-card>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+import ProfileMoviesFilter from "./ProfileMoviesFilter";
+export default {
+  name: "ProfileMoviesList",
+  components: { ProfileMoviesFilter }
+};
+</script>
+
+<style scoped>
+.oneRow {
+  width: 100%;
+}
+</style>
